815729fed1f0050a77e3562afb628009cffee2d8
subah19 <103736445+subah19@users.noreply.github.com>
2022-09-14 18:12:32 +0530

    pull tutorial 1

--- tut01/tut01.py
+++ tut01/tut01.py
@@ -1,6 +1,14 @@
-from math import factorial
+def octact_identification(mod=5000):
+###Code
 
 
-num=1
-num=int(input("Enter the number to find factorial"))
-print("The factorial of number",num, "is",factorial(num),)
+from platform import python_version
+ver = python_version()
+
+if ver == "3.8.10":
+    print("Correct Version Installed")
+else:
+    print("Please install 3.8.10. Instruction are present in the GitHub Repo/Webmail. Url: https://pastebin.com/nvibxmjw")
+
+mod=5000
+octact_identification(mod)
